e4eb878d81aa422c4e46efaf9bcc44a0f9efd109
NusratTisha7 <nusratjahantisha763@gmail.com>
2022-08-28 01:42:53 +0600

    cart,payment update

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -3,21 +3,26 @@ const query = require('../config/db')
 module.exports.addCartItem = async (req, res) => {
     try {
         let { userID, productID, count } = req.body
+        let price;
         let getPriceSql = "SELECT * FROM product WHERE id = ?";
-        let [result]=await query(getPriceSql,[productID])
+        let [result] = await query(getPriceSql, [productID])
         price = result.price
-        let sql = "CREATE TABLE IF NOT EXISTS cart (id INT AUTO_INCREMENT PRIMARY KEY, price int, count int, userID int, FOREIGN KEY (userID) REFERENCES user(id),productID int, FOREIGN KEY (productID) REFERENCES product(id),isActive BOOLEAN)";
-        await query(sql).then(async response => {
-            let sql = "INSERT INTO cart (userID,productID,price,count,isActive) VALUES ?";
-            let values = [[userID, productID, price, count,1]]
-            await query(sql, [values]).then(response => {
-                return res.status(200).send({ status: 1, message: 'Added to cart successfully!' })
+        if (price) {
+            let sql = "CREATE TABLE IF NOT EXISTS cart (id INT AUTO_INCREMENT PRIMARY KEY, price int, count int, userID int, FOREIGN KEY (userID) REFERENCES user(id),productID int, FOREIGN KEY (productID) REFERENCES product(id),isActive BOOLEAN)";
+            await query(sql).then(async response => {
+                let sql = "INSERT INTO cart (userID,productID,price,count,isActive) VALUES ?";
+                let values = [[userID, productID, price, count, 1]]
+                await query(sql, [values]).then(response => {
+                    return res.status(200).send({ status: 1, message: 'Added to cart successfully!' })
+                }).catch(err => {
+                    return res.status(400).send({ status: 0, message: 'Something failed!' });
+                })
             }).catch(err => {
                 return res.status(400).send({ status: 0, message: 'Something failed!' });
             })
-        }).catch(err => {
+        } else {
             return res.status(400).send({ status: 0, message: 'Something failed!' });
-        })
+        }
     } catch (err) {
         return res.status(400).send({ status: 0, msg: err })
     }
@@ -67,8 +72,8 @@ module.exports.deleteCartItem = async (req, res) => {
 
 module.exports.editActiveStatus = async (req, res) => {
     try {
-        let { activeStatus,id } = req.body
-        let sql = "UPDATE cart SET isActive = " + activeStatus + " WHERE id = "+id+"";
+        let { activeStatus, id } = req.body
+        let sql = "UPDATE cart SET isActive = " + activeStatus + " WHERE id = " + id + "";
         await query(sql).then(response => {
             return res.status(200).send({ status: 1, message: 'Successfully updated' })
         }).catch(err => {
